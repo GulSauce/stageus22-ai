bec9e24108ac34faf4e9032a3501b4aa3e72231e
Oh YoungJe <pine7420@naver.com>
2025-01-18 15:58:51 +0900

    Style: respone dto 프로퍼티 ㄱ수정

--- app/dto/response/grade_text_response.py
+++ app/dto/response/grade_text_response.py
@@ -4,4 +4,4 @@ from typing import List
 from app.dto.model.quiz import  Quiz
 
 class GradeTextResponse(BaseModel):
-    score: List[int] = Field(description="Scores of the answers")
+    scores: List[int] = Field(description="Scores of the answers")
