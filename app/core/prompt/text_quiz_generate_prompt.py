d302d278a7dd64b193c39d2a61088f2f84f8427c
Oh YoungJe <pine7420@naver.com>
2025-02-12 15:03:21 +0900

    프롬프트 개선

--- app/core/prompt/text_quiz_generate_prompt.py
+++ app/core/prompt/text_quiz_generate_prompt.py
@@ -3,6 +3,7 @@ from langchain.prompts import PromptTemplate
 text_quiz_generation_prompt = PromptTemplate(
     template="""
         당신은 퀴즈 생성 전문가입니다. 퀴즈를 생성해주세요
+        사용자 입장에서는 참고자료가 무엇인지 모르기 때문에 참고자료임을 밝히지 마세요
         
         ## 퀴즈 종류
         주관식 퀴즈
