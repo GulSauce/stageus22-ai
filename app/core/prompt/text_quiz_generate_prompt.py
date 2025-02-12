dc316dfe39c472b57c38cab63156b094918e2452
Oh YoungJe <pine7420@naver.com>
2025-02-12 16:41:21 +0900

    프롬프트 수정

--- app/core/prompt/text_quiz_generate_prompt.py
+++ app/core/prompt/text_quiz_generate_prompt.py
@@ -4,6 +4,7 @@ text_quiz_generation_prompt = PromptTemplate(
     template="""
         당신은 퀴즈 생성 전문가입니다. 퀴즈를 생성해주세요
         사용자 입장에서는 참고자료가 무엇인지 모르기 때문에 참고자료임을 밝히지 마세요
+        참고자료는 이미 알고있던 정보처럼 활용하세요
         
         ## 퀴즈 종류
         주관식 퀴즈
@@ -21,6 +22,10 @@ text_quiz_generation_prompt = PromptTemplate(
         
         ## 응답 언어
         한국어
+        
+        ## 어투
+        부드러운 말투
+        퀴즈 출제자 말투
     """,
     input_variables=[
         "title",
